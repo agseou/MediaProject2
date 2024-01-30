62d453242a949dccafb70f31222df81912716f95
SeoU <tjdnfkd1225@naver.com>
2024-01-30 17:30:43 +0900

    Initial Commit

---
+++ MediaProject2/Scene/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  MediaProject2
+//
+//  Created by 은서우 on 2024/01/30.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
